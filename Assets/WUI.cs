aa6ed6d49a8cdb96e5e265f291d4554ce5300426
Pereee <68907855+Peree-x@users.noreply.github.com>
2021-06-14 23:00:38 +0200

    Inventory WUI half finished

--- Assets/WUI.cs
+++ Assets/WUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class WUI : MonoBehaviour
 {
@@ -10,13 +11,46 @@ public class WUI : MonoBehaviour
     [SerializeField] private GameObject Button1;
     [SerializeField] private GameObject Button2;
     [SerializeField] private GameObject Button3;
-    bool Populated1;
-    bool Populated2;
-    bool Populated3;
+    [SerializeField] private TextMeshProUGUI text1;
+    [SerializeField] private TextMeshProUGUI text2;
+    [SerializeField] private TextMeshProUGUI text3;
+    bool[] Populated;
+    bool[] bol;
     private Sprite icon;
     private void Update()
     {
-        foreach (Transform Item in GameObject.Find("Aim").transform)
+        CheckSetGo();
+//        for(int i = 0; i > Populated.Length ; i++)
+//        {
+//            if(Populated[i] != true)
+//            {
+//                remove(i);
+//            }
+//        }
+//        proveri dali je idalje true i ako jeste onda mu ne radi nista a ako nije onda upotrebi funkciju remove();
+    }
+    void remove(int i)
+    {
+        switch(i)
+        {
+            case 0:
+                //image remove
+                text1.text = null;
+            break;
+            case 1:
+                //image remove
+                text2.text = null;
+            break;
+            case 2:
+                //image remove
+                text3.text = null;
+            break;
+        }
+
+    }
+    void CheckSetGo()
+    {
+        foreach (Transform Item in GameObject.FindWithTag("Aim").transform)
         {
             if(Item.gameObject.tag == "Weapon")
             {
@@ -24,26 +58,21 @@ public class WUI : MonoBehaviour
                 {
                     case "Sword":
                         //change icon wim.Get(Item.name).icon;
-                        Text text1;
-                        Populated1 = true;
-                        text1 = Button1.GetComponentInChildren<Text>();
+                        Populated[0] = true;
                         text1.text = wim.Get(Item.name).name;
 
                     break;
                     case "Pistol":
                         //change icon wim.Get(Item.name).icon;
-                        Text text2;
-                        Populated2 = true;
-                        text2 = Button2.transform.Find("Text").gameObject.GetComponent<Text>();
+                        Populated[1] = true;
                         text2.text = wim.Get(Item.name).name;
+                        Debug.Log("pistol");
                         //Stavi description ako bude hover 
 
                     break;
                     case "Gun":
                         //change icon wim.Get(Item.name).icon;
-                        Text text3;
-                        Populated3 = true;
-                        text3 = Button3.GetComponentInChildren<Text>();
+                        Populated[2] = true;
                         text3.text = wim.Get(Item.name).name;
                     break;
                     default:
